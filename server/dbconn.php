df1be6b4765ea0e95aa84df77dcd1e43871c1d18
Barath <kvbarath2004@gmail.com>
2024-08-31 19:24:49 +0530

    DB connection established

---
+++ server/dbconn.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "ClimateControlSystem";
+$conn = mysqli_connect($host, $user, $password, $dbname);
+?>
---
+++ server/index.php
@@ -0,0 +1,30 @@
+<?php
+ include('dbconn.php'); 
+ $query = "SELECT * FROM data";
+ $results = mysqli_query($conn,$query);
+ ?>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
+
+        <!-- DATATABLES -->
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
+
+        <!-- CHART JS -->
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+        <title>Sensor Data</title>
+    </head>
+    <body style="padding-top:20px;">
+        
+    </body>
+</html>
